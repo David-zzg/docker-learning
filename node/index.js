4c701a0baf288172bc07d3fc009181f3b24a0950
David <87726263@qq.com>
2018-09-23 13:49:53 +0800

    What:干了什么 Why: *为什么这么做

--- node/index.js
+++ node/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!ddd23');
+  res.send('Hello World!david');
 });
 
 var server = app.listen(3000, function () {
